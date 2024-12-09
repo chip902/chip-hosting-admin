455a5ff93e136c89b16b7f6b44a473b1f4680d90
chip902 <68761150+chip902@users.noreply.github.com>
2024-12-08 23:46:40 -0500

    introduce sorting to data API

--- app/api/data/route.ts
+++ app/api/data/route.ts
@@ -4,9 +4,16 @@ import prisma from "@/prisma/client";
 export async function GET() {
 	try {
 		const customers = await prisma.customer.findMany();
+		customers.sort((a, b) => a.name.localeCompare(b.name));
+
 		const projects = await prisma.project.findMany();
+		projects.sort((a, b) => a.name.localeCompare(b.name));
+
 		const tasks = await prisma.task.findMany();
+		tasks.sort((a, b) => a.name.localeCompare(b.name));
+
 		const users = await prisma.user.findMany();
+
 		return NextResponse.json({ customers, projects, tasks, users }, { status: 200 });
 	} catch (error) {
 		return NextResponse.json({ error: "Error fetching data" }, { status: 500 });
