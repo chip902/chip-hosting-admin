bc2b9a694c33b5e5e5e7ea2b935436eac99bfc9d
chip902 <68761150+chip902@users.noreply.github.com>
2025-03-07 08:44:00 -0500

    chore: update packages

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en" className="h-full antialiased [--removed-body-scroll-bar-size:0px]">
+		<html lang="en" suppressHydrationWarning className="h-full antialiased [--removed-body-scroll-bar-size:0px]">
 			<body className="h-full">
 				<Providers>
 					<MainLayout>{children}</MainLayout>
